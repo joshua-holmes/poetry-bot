c669f857e9a3727971bf7da816bab83c9fe99eab
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-02-26 00:22:42 -0800

    Add ping endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
-    routing::post,
+    routing::{get, post},
     Json, Router,
 };
 mod schemas;
@@ -15,7 +15,9 @@ const PORT: u16 = 3000;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api", post(root));
+    let app = Router::new()
+        .route("/api", post(api))
+        .route("/ping", get(ping));
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", PORT))
         .await
         .unwrap_or_else(|e| panic!("Axum failed to bind to port {}:\n{}", PORT, e));
@@ -25,9 +27,13 @@ async fn main() {
         .expect("Failed to start Axum server");
 }
 
-async fn root(Json(payload): Json<ClaraRequest>) -> (StatusCode, Response) {
+async fn api(Json(payload): Json<ClaraRequest>) -> (StatusCode, Response) {
     match ask_clara(payload).await {
         Ok(resp) => (StatusCode::OK, Json(resp).into_response()),
         Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, Json(err).into_response()),
     }
+}
+
+async fn ping() -> (StatusCode, &'static str) {
+    (StatusCode::OK, "Pong!")
 }
